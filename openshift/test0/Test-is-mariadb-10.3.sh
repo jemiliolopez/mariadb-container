2fcb7e6564e63766b149e228d77cb60bc7b71fb2
jemiliolopez <jemiliolopezalvarez@gmail.com>
2021-01-09 10:25:19 +0100

    all

--- openshift/test0/Test-is-mariadb-10.3.sh
+++ openshift/test0/Test-is-mariadb-10.3.sh
@@ -1,36 +1,7 @@
-#!/bin/bash
-
-## oc new-project imagenes-wordpress  --description="Crea las imagenes de mysql8 y wp" --display-name="Imagenes-Wordpress"
-
-## ssh-keygen -f /home/origin/.ssh/id_rsa -b 2048 -t rsa -q -N '' -m pem -C 'ctrl.srv.lab'
-## oc secrets new-sshauth github-secret-pc --ssh-privatekey='/home/emilio/.ssh/id_rsa'
-
-## use oc create secret github-secret-jemiliolopez
-
-## oc secrets link builder github-secret-pc
 
 
-
-## ssh-keygen -f /home/origin/.ssh/id_rsa -b 2048 -t rsa -q -N '' -m pem -C 'ctrl.srv.lab'
-## oc create secret github-secret-jemiliolopez --ssh-privatekey='/home/emilio/.ssh/id_rsa'
-## oc secrets link builder github-secret-jemiliolopez
-
-
-
-# oc apply -f wp-builds/PVs/os-nfs-wpp-pv08.json -f wp-builds/PVs/os-nfs-masterp-pv07.json -f wp-builds/PVs/os-nfs-slave1-pv06.json -f wp-builds/PVs/os-nfs-slave2-pv05.json
-
-# sleep 5
-
-# oc apply -f wp-builds/PVs/os-nfs-wpp-pv08.json -f wp-builds/PVs/os-nfs-masterp-pv07.json -f wp-builds/PVs/os-nfs-slave1-pv06.json -f wp-builds/PVs/os-nfs-slave2-pv05.json && sleep 10 && \
-
-
-. ./Env-mariadb-10.3.sh
-
-oc process -f ./../build/Test-is-mariadb-10.3.json \
-   -p PROJECT_NAME="$PROJECT_NAME" \
-   -p APP_NAME="$APP_NAME" \
-    | oc apply -f -
-
+oc process -f ./../build/Test-is-mariadb-10.3.json --param-file Env-mariadb-10.3.params | oc apply -f -
+   
 oc rollout status dc/${APP_NAME}
 
 oc set env  dc/${APP_NAME} MYSQL_USER- MYSQL_PASSWORD- MYSQL_DATABASE- MYSQL_ROOT_PASSWORD-
