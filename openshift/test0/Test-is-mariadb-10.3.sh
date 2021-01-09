761b7bea1039b794b081136f03fce634bd673f58
jemiliolopez <jemiliolopezalvarez@gmail.com>
2021-01-09 10:36:10 +0100

    all

--- openshift/test0/Test-is-mariadb-10.3.sh
+++ openshift/test0/Test-is-mariadb-10.3.sh
@@ -1,8 +1,6 @@
-
-
 oc process -f ./../build/Test-is-mariadb-10.3.json --param-file Env-mariadb-10.3.params | oc apply -f -
    
-oc rollout status dc/${APP_NAME}
+# oc rollout status dc/${APP_NAME}
 
-oc set env  dc/${APP_NAME} MYSQL_USER- MYSQL_PASSWORD- MYSQL_DATABASE- MYSQL_ROOT_PASSWORD-
+# oc set env  dc/${APP_NAME} MYSQL_USER- MYSQL_PASSWORD- MYSQL_DATABASE- MYSQL_ROOT_PASSWORD-
 
