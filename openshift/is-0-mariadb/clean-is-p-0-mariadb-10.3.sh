c4d66f13f2e498b668f7d3e23ad96a4de56baeb9
jemiliolopez <jemiliolopezalvarez@gmail.com>
2021-01-09 10:09:15 +0100

    all

---
+++ openshift/is-0-mariadb/clean-is-p-0-mariadb-10.3.sh
@@ -0,0 +1,1 @@
+oc process -f ./../build/is-0-mariadb-10.3.json --param-file Env-is-0-mariadb-10.3.params | oc delete -f -
---
+++ openshift/is-0-mariadb/is-p-0-mariadb-10.3.sh
@@ -0,0 +1,4 @@
+oc process -f ./../build/is-0-mariadb-10.3.json --param-file Env-is-0-mariadb-10.3.params | oc apply -f -
+
+
+   
