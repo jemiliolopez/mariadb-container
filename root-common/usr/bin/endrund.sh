b9a0fe09cbf1697b0fb2aaa50f7910077f7e98ed
jemiliolopez <jemiliolopezalvarez@gmail.com>
2021-01-18 07:38:20 +0100

    all

--- root-common/usr/bin/endrund.sh
+++ root-common/usr/bin/endrund.sh
@@ -2,8 +2,8 @@
 
 sleep 5
 
-DB_PASSWORD=''
-DB_ROOT_PASSWORD=''
+# DB_PASSWORD=''
+# DB_ROOT_PASSWORD=''
 sleep 5
-BASH_ENV=/etc/environment
+# BASH_ENV=/etc/environment
 exec ${MYSQL_PREFIX}/libexec/mysqld --defaults-file=$MYSQL_DEFAULTS_FILE "$@" 2>&1
