e60ca7766ab0867bf513f4c06eeb8763b56f3cf2
jemiliolopez <jemiliolopezalvarez@gmail.com>
2021-02-02 11:03:39 +0100

    06_Set_environement
    Ponemos las variables de la base de datos en openshift/contexto/mysql-pre-init/10-get-variables.sh, quedando de la siguiente manera:
    
    get-variables() {
        export MYSQL_ROOT_PASSWORD="$(cat '/tmp/mysql/shared/secrets/mysql-root-password ')"
        # export MYSQL_OPERATIONS_USER="$(cat '/tmp/mysql/shared/cfmap/mysql-operations-user')"
        # export MYSQL_OPERATIONS_PASSWORD="$(cat '/tmp/mysql/shared/secrets/mysql-operations-password ')"
        export MYSQL_USER="$(cat '/tmp/mysql/shared/cfmap/mysql-user')"
        export MYSQL_PASSWORD="$(cat '/tmp/mysql/shared/secrets/mysql-password ')"
        export MYSQL_DATABASE="$(cat '/tmp/mysql/shared/cfmap/mysql-database')"
        rm -fR  '/tmp/mysql/shared'
    }
    
    if ! [ -v MYSQL_RUNNING_AS_SLAVE ]; then
        get-variables
    fi
    
    	modificado:     openshift/1-test/1-Test-is-mariadb103.json5
    	modificado:     openshift/build/1-Test-is-mariadb103.json
    	nuevo archivo:  openshift/contexto/mysql-pre-init/10-get-variables.sh

---
+++ openshift/contexto/mysql-pre-init/10-get-variables.sh
@@ -0,0 +1,15 @@
+get-variables() {
+    export MYSQL_ROOT_PASSWORD="$(cat '/tmp/mysql/shared/secrets/mysql-root-password')"
+    # export MYSQL_OPERATIONS_USER="$(cat '/tmp/mysql/shared/cfmap/mysql-operations-user')"
+    # export MYSQL_OPERATIONS_PASSWORD="$(cat '/tmp/mysql/shared/secrets/mysql-operations-password')"
+    export MYSQL_USER="$(cat '/tmp/mysql/shared/cfmap/mysql-user')"
+    export MYSQL_PASSWORD="$(cat '/tmp/mysql/shared/secrets/mysql-password')"
+    export MYSQL_DATABASE="$(cat '/tmp/mysql/shared/cfmap/mysql-database')"
+    rm -fR  '/tmp/mysql/shared'
+}
+
+if ! [ -v MYSQL_RUNNING_AS_SLAVE ]; then
+    get-variables
+fi
+
+
