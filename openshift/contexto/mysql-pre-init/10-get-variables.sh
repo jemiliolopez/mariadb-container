c82b9502d7160a732c917d9139328bbbf2f74389
jemiliolopez <jemiliolopezalvarez@gmail.com>
2021-02-12 10:50:30 +0100

    Fix 10-get-variables.sh
    Comento:
    
      # rm -fR  '/tmp/mysql/shared'
    
    para que pueda ser usado por los contenedores de wordpress.
    
    modificado:     ../openshift/contexto/mysql-pre-init/10-get-variables.sh

--- openshift/contexto/mysql-pre-init/10-get-variables.sh
+++ openshift/contexto/mysql-pre-init/10-get-variables.sh
@@ -3,7 +3,7 @@ get-variables() {
     export MYSQL_USER="$(cat '/tmp/mysql/shared/cfmap/mysql-user')"
     export MYSQL_PASSWORD="$(cat '/tmp/mysql/shared/secrets/mysql-password')"
     export MYSQL_DATABASE="$(cat '/tmp/mysql/shared/cfmap/mysql-database')"
-    rm -fR  '/tmp/mysql/shared'
+    # rm -fR  '/tmp/mysql/shared'
 }
 
 if ! [ -v MYSQL_RUNNING_AS_SLAVE ]; then
